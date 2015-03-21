89d4c2fa2b74d47c4e8e80dd87c56b39d8b198aa
Thomas Levine <_@thomaslevine.com>
2015-03-21 16:35:20 +0000

    model

--- portal/model.py
+++ portal/model.py
@@ -9,18 +9,18 @@ from .article import reify
 cache = {}
 
 class Article:
-    @classmethod
+    @staticmethod
     def one(article_dir, endpoint):
         if endpoint not in cache:
             cache[endpoint] = reify(article_dir, endpoint)
         return cache[endpoint]
 
-    @classmethod
+    @staticmethod
     def many(article_dir, topdir = '!'):
         endpoints = (os.path.join(topdir, x) for x in os.listdir(os.path.join(article_dir, topdir)))
         return (Article.one(article_dir, endpoint) for endpoint in sorted(endpoints))
 
-    @classmethod
+    @staticmethod
     def all(article_dir):
         for topdir in os.listdir(article_dir):
             yield from Article.many(os.path.join(article_dir, topdir))
@@ -58,7 +58,8 @@ class ArticleHitCounts(Base):
     __tablename__ = 'article_statistics'
 
     # With the trailing slash
-    endpoint = s.Column(s.String)
+    endpoint = s.Column(s.String, primary_key = True)
+
     day_0 = s.Column(s.Integer, nullable = False) # today
     day_1 = s.Column(s.Integer, nullable = False) # yesterday
     day_2 = s.Column(s.Integer, nullable = False)
