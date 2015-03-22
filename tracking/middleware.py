f1be89fb9045119afa275ba9e035693f89127dc2
Thomas Levine <_@thomaslevine.com>
2015-03-22 16:40:21 +0000

    yay

--- tracking/middleware.py
+++ tracking/middleware.py
@@ -1,10 +1,12 @@
 from .models import Hit
+from .context_processors import rand
 
 class TrackingMiddleware:
-    def process_request(self, request, response):
+    def process_request(self, request):
         'Generate the hit identifier.'
         request.hit_id = rand()
+
     def process_response(self, request, response):
         'Get the status code.'
-        Hit.objects.filter(hit = request.hit_id).update(status_code = response.status_code))
+        Hit.objects.filter(hit = request.hit_id).update(status_code = response.status_code)
         return response
