c8eb9137669a5d049eb9303cfeb8821e9dea1ad6
Thomas Levine <_@thomaslevine.com>
2015-03-22 15:32:21 +0000

    -

--- tracking/middleware.py
+++ tracking/middleware.py
@@ -12,14 +12,19 @@ class TrackingMiddleware:
     The hit_id should be rendered in the template so it can be passed
     along in the XHR.
     '''
-    def process_template_response(self, request, response):
+    def process_response(self, request, response):
         if 'session_id' not in request.session:
             request.session['session_id'] = rand()
         hit_id = rand()
-        response.context_data['hit_id'] = hit_id
         Hit.objects.create(hit = hit_id,
                            session = request.session['session_id'],
                            ip_address = request.META['REMOTE_ADDR'],
                            user_agent = request.META['HTTP_USER_AGENT'],
                            referrer = request.META.get('HTTP_REFERER', ''))
+        print(hit_id)
+        if hasattr(response, 'context_data'):
+            print(response.context_data)
+            response.context_data['hit_id'] = self.hit_id
+  #     response.context_data['hit_id'] = self.hit_id
         return response
+
