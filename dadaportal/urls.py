8179f4c750261b94b89f75793dc61fa5e004ab4a
Thomas Levine <_@thomaslevine.com>
2015-03-22 01:20:59 +0000

    urls

--- dadaportal/urls.py
+++ dadaportal/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -8,8 +9,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'dadaportal.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
     url(r'^admin/', include(admin.site.urls)),
+
+    # https://docs.djangoproject.com/en/dev/howto/static-files/#serving-files-uploaded-by-a-user-during-development
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 
     url(r'^\+$', RedirectView.as_view(url='/+/')),
     url(r'^\+/$', 'search.views.search'),
