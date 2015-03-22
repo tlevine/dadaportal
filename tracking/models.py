030bbc79c57481a5cdb5a26ac20e04e96503eaee
Thomas Levine <_@thomaslevine.com>
2015-03-22 04:23:23 +0000

    models

--- tracking/models.py
+++ tracking/models.py
@@ -6,16 +6,16 @@ class Hit(models.Model):
 
     # Populated on the first request
     id = models.BigIntegerField(primary_key = True)
-    session = s.Column(nullable = False)
-    datetime = s.Column(s.DateTime, nullable = False)
-    ip_address = s.Column(nullable = False)
-    user_agent = s.Column(s.String, nullable = False)
-    referrer = s.Column(s.String, nullable = False) # blank if none
+    session = models.Field(null = False)
+    datetime = models.Field(s.DateTime, null = False)
+    ip_address = models.Field(null = False)
+    user_agent = models.Field(s.String, null = False)
+    referrer = models.Field(s.String, null = False) # blank if none
 
     # Populated on the subsequent XHR, updated every few seconds
-    screen_width = s.Column(s.Integer, nullable = True)
-    screen_height = s.Column(s.Integer, nullable = True)
-    seconds_on_page = s.Column(s.Integer, nullable = True)
+    screen_width = models.Field(s.Integer, null = True)
+    screen_height = models.Field(s.Integer, null = True)
+    seconds_on_page = models.Field(s.Integer, null = True)
 
     def insert_primary():
         pass
@@ -23,7 +23,7 @@ class Hit(models.Model):
     def insert_js():
         pass
 
-class ArticleHitCounts(Base):
+class ArticleHitCounts(models.Model):
     '''
     This table has a row for each article and hour, and a column for hit
     counts for the past few days. It can be generated fully the contents
@@ -32,16 +32,16 @@ class ArticleHitCounts(Base):
     __tablename__ = 'article_statistics'
 
     # With the trailing slash
-    endpoint = s.Column(s.String, primary_key = True)
+    endpoint = models.TextField(primary_key = True)
 
-    day_0 = s.Column(s.Integer, nullable = False) # today
-    day_1 = s.Column(s.Integer, nullable = False) # yesterday
-    day_2 = s.Column(s.Integer, nullable = False)
-    day_3 = s.Column(s.Integer, nullable = False)
-    day_4 = s.Column(s.Integer, nullable = False)
-    day_5 = s.Column(s.Integer, nullable = False)
-    day_6 = s.Column(s.Integer, nullable = False) # a week ago, rounded down
-    day_7 = s.Column(s.Integer, nullable = False) # a week ago, rounded up
+    day_0 = models.IntegerField(null = False) # today
+    day_1 = models.IntegerField(null = False) # yesterday
+    day_2 = models.IntegerField(null = False)
+    day_3 = models.IntegerField(null = False)
+    day_4 = models.IntegerField(null = False)
+    day_5 = models.IntegerField(null = False)
+    day_6 = models.IntegerField(null = False) # a week ago, rounded down
+    day_7 = models.IntegerField(null = False) # a week ago, rounded up
 
     @classmethod
     def shift(Klass):
@@ -49,13 +49,13 @@ class ArticleHitCounts(Base):
         pass
 
 
-class Search(Base):
+class Search(models.Model):
     '''
     This table has a row for each article and a column for each of several
     statistics. It can be generated fully from the contents of the "hit" table.
     '''
     __tablename__ = 'search'
-    hit = s.Column(s.BigInteger, s.ForeignKey(Hit.id), primary_key = True)
-    terms = s.Column(s.String, nullable = False)
-    email_only = s.Column(s.Boolean, nullable = False)
-    n_results = s.Column(s.Integer, nullable = False)
+    hit = models.BigIntegerField(primary_key = True)
+    terms = models.TextField(null = False)
+    email_only = models.BooleanField(null = False)
+    n_results = models.IntegerField(null = False)
