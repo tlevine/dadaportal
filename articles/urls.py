40598684fab8e7b6cd9a2e48a62dce8117f20fd2
Thomas Levine <_@thomaslevine.com>
2015-03-22 02:24:10 +0000

    static

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
-from .views import article_dynamic
+from .views import article
 
 urlpatterns = patterns('',
-    url(r'^([^/]+)/$', article_dynamic),
+    url(r'^(.+)/$', article),
+    url(r'^(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ARTICLES_DIR}),
 
     # Something other than 404?
    #url(r'^$',
--- articles/views.py
+++ articles/views.py
@@ -1,27 +1,15 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.http import Http404
 
 from .models import ArticleCache
 
-# Create your views here.
-#@app.route('/<endpoint:path>/')
-def article_dynamic(request, slug):
+def article(request, endpoint):
     try:
-        a = ArticleCache.objects.get(endpoint = '!/' + slug)
+        a = ArticleCache.objects.get(endpoint = endpoint)
     except ArticleCache.DoesNotExist:
         raise Http404('No such article')
     else:
         params = a.head()
         params['body'] = a.body
         return render(request, 'article.html', params)
-
-#@app.route('/<endpoint:path>')
-def article_static(endpoint):
-    x = endpoint.lstrip('/.')
-    if os.path.isfile(os.path.join(ARTICLE_DIR, x)):
-        return static_file(endpoint, root = ARTICLE_DIR)
-    elif os.path.isfile(os.path.join(STATIC_DIR, x)):
-        # For backwards compatibility
-        return static_file(endpoint, root = STATIC_DIR)
-    else:
-        redirect('/%s/' % endpoint)
