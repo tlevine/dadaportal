e45e86b7190f99debafef6337baeb434c7861444
Thomas Levine <_@thomaslevine.com>
2015-03-22 07:22:48 +0000

    -

--- tracking/models.py
+++ tracking/models.py
@@ -8,51 +8,23 @@ class Hit(models.Model):
     # Populated on the first request
     id = models.BigIntegerField(primary_key = True)
     session = models.BigIntegerField(null = False)
-    datetime = models.DateTimeField(null = False, default = datetime.datetime.now)
+    datetime_start = models.DateTimeField(null = False, default = datetime.datetime.now)
     endpoint = models.TextField(null = False)
     ip_address = models.IPAddressField(null = False)
     user_agent = models.TextField(null = False)
     referrer = models.URLField(null = False)
 
     # Populated on the subsequent XHR, updated every few seconds
-    fresh = models.BooleanField(null = False, default = True)
-    screen_width = models.Field(null = True)
-    screen_height = models.Field(null = True)
-    seconds_on_page = models.Field(null = True)
-
-class ArticleHitCounts(models.Model):
-    '''
-    This table has a row for each article and hour, and a column for hit
-    counts for the past few days. It can be generated fully from the contents
-    of the "hit" table.
-    '''
-    # With the trailing slash
-    endpoint = models.TextField(primary_key = True)
-
-    day_0 = models.IntegerField(null = False) # today
-    day_1 = models.IntegerField(null = False) # yesterday
-    day_2 = models.IntegerField(null = False)
-    day_3 = models.IntegerField(null = False)
-    day_4 = models.IntegerField(null = False)
-    day_5 = models.IntegerField(null = False)
-    day_6 = models.IntegerField(null = False) # a week ago, rounded down
-    day_7 = models.IntegerField(null = False) # a week ago, rounded up
-
-    @classmethod
-    def shift(Klass):
-        'Klass.day_7 is set to Klass.day_6, and so on.'
-        for i in reversed(range(1, 8)):
-            yesterday = 'day_%d' % i
-            today = 'day_%d' % (i - 1)
-            kwargs = {yesterday: models.F(today)}
-            Klass.objects.all().update(**kwargs)
+    screen_width = models.IntegerField(null = True)
+    screen_height = models.IntegerField(null = True)
+    datetime_end = models.DateTimeField(null = True)
 
 class Search(models.Model):
     '''
     This table has a row for each article and a column for each of several
     statistics. It can be generated fully from the contents of the "hit" table.
     '''
-    hit = models.BigIntegerField(primary_key = True)
+    hit = models.ForeignKey(Hit, primary_key = True)
     terms = models.TextField(null = False)
     email_only = models.BooleanField(null = False)
     n_results = models.IntegerField(null = False)
--- tracking/views.py
+++ tracking/views.py
@@ -1,46 +1,49 @@
-import logging
+import logging, datetime
 
-from .models import Hit, Search, ArticleHitCounts
+from .models import Hit, Search
 
 logger = logging.getLogger(__name__)
 
 def track_nonsearch(request):
     '''
     Begin the tracking on the fundamental (probably HTML) page load.
+
+    The hit_id should be rendered in the template so it can be passed
+    along in the XHR.
     '''
     if 'session_id' not in request.session:
         request.session['session_id'] = random.getrandbits(128)
-    session = request.session['session_id']
-    hit = Hit.objects.create(session = session,
+    hit_id = random.getrandbits(128)
+
+    hit = Hit.objects.create(hit = hit_id,
+                             session = request.session['session_id'],
                              ip_address = request.META.REMOTE_ADDR,
                              user_agent = request.META.HTTP_USER_AGENT,
                              referrer = request.META.HTTP_REFERER)
 
-def track_search(request, search_terms
-    Search.objects.create(hit = hit.id, terms = search_terms,
+    return hit_id
 
+def track_search(request, search_terms:str, email_only:bool, n_results:int):
+    hit_id = track_nonsearch(request)
+    Search.objects.create(hit = hit_id, terms = search_terms,
+                          email_only = email_only, n_results = n_results)
+    return hit_id
 
 
 XHR_FIELDS = [
-    'screen_width', 'screen_height', 'seconds_on_page',
+    'screen_width', 'screen_height',
 ]
 def track_xhr(request):
     '''
     Finish the tracking after the XHR.
     '''
     try:
-        hit = Hit.objects.get(session = request.session['session_id'])
+        hit = Hit.objects.get(hit = request.session['hit_id'])
     except Hit.DoesNotExist:
         logger.warn('Session "%d" was missing.' % request.session['session_id'])
         return
 
-    if not hit.fresh:
-        hit.fresh = False
-        hitcount = ArticleHitCounts.get_or_create(endpoint = hit.endpoint)
-        hitcount.day_0 += 1
-        hitcount.save()
-
     for field in XHR_FIELDS:
         setattr(hit, field, request.POST.get(field))
-
+    hit.datetime_end = datetime.datetime.now()
     hit.save()
