8b1fb35714757be83dec47210f9c3b120d75adf6
Thomas Levine <_@thomaslevine.com>
2015-03-22 08:45:55 +0000

    -

--- templates/track.js
+++ templates/track.js
@@ -1,24 +1,29 @@
 (function() {
   var interval = 5 * 1000 // milliseconds
   send('{{ csrf_token }}')
-  console.log('{{csrf_token}}')
 
   function send(csrfmiddlewaretoken) {
     var f = new FormData()
     f.append('hit_id', '{{hit_id}}')
     f.append('csrfmiddlewaretoken', csrfmiddlewaretoken)
-    f.append('screen_width', 33)
-    f.append('screen_height', 22)
+
+    f.append('availWidth', window.screen.availWidth)
+    f.append('availHeight', window.screen.availHeight)
+    f.append('scrollMaxX', window.screen.scrollMaxX)
+    f.append('scrollMaxY', window.screen.scrollMaxY)
+    f.append('pageXOffset', window.screen.pageXOffset)
+    f.append('pageYOffset', window.screen.pageYOffset)
 
     var r = new XMLHttpRequest();
     r.open('POST', '/track', true)
     r.onreadystatechange = receive
     r.send(f)
-  }
 
-  function receive() {
-    if (r.readyState==4) {
-      setTimeout(function() {send(r.responseText)}, interval)
+    function receive() {
+      if (r.readyState==4) {
+        setTimeout(function() {send(r.responseText)}, interval)
+      }
     }
   }
+
 })()
--- tracking/views.py
+++ tracking/views.py
@@ -7,28 +7,32 @@ from .models import Hit
 
 logger = logging.getLogger(__name__)
 
-XHR_FIELDS = [
-    'screen_width', 'screen_height',
-]
 @csrf_exempt
 def track_xhr(request):
     '''
     Finish the tracking after the XHR.
     '''
     if 'hit_id' not in request.POST:
-        print(1)
         return HttpResponse(status = 403)
     hit_id = request.POST['hit_id']
     try:
         hit = Hit.objects.get(hit = hit_id)
     except Hit.DoesNotExist:
-        print(2)
         logger.warn('Hit "%d" was missing.' % hit_id)
         return HttpResponse(status = 403)
 
-    for field in XHR_FIELDS:
+    for field in ['availWidth', 'availHeight']:
         setattr(hit, field, request.POST.get(field))
+    for field in ['scrollMaxX', 'scrollMaxY', 'pageXOffset', 'pageYOffset']:
+        old = request.POST.get(field)
+        new = getattr(hit, field)
+        if old == None:
+            setattr(hit, field, new)
+        elif new == None:
+            pass
+        else:
+            setattr(hit, field, max(old, new))
+
     hit.datetime_end = datetime.datetime.now()
     hit.save()
-    print(3)
     return HttpResponse(status=204)
