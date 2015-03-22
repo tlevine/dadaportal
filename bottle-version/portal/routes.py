b3e49b37e0178a3f8faca15733183562dbf29c13
Thomas Levine <_@thomaslevine.com>
2015-03-22 03:28:59 +0000

    / page

--- bottle-version/portal/routes.py
+++ bottle-version/portal/routes.py
@@ -1,61 +1,8 @@
-import os, re
-
-from unidecode import unidecode
-from lxml.html.clean import clean_html
-from bottle import (
-    Bottle, request, response, abort, redirect, view, TEMPLATE_PATH,
-    template, static_file,
-)
-
-from .model import Article
-from .article import is_static as article_is_static
-
-PORTAL_DIR = os.path.abspath(os.path.split(os.path.split(__file__)[0])[0])
-TEMPLATE_PATH.append(os.path.join(PORTAL_DIR, 'views'))
-ARTICLE_DIR = os.path.join(PORTAL_DIR, 'articles')
-STATIC_DIR = os.path.join(PORTAL_DIR, 'static')
-ARTICLE_NOTMUCH_FROM = 'replace-this-with-a-random-thing-for-security-reasons'
-app = Bottle()
-
-#@app.hook('before_request')
-#def strip_path():
-#    request.environ['PATH_INFO'] = request.environ['PATH_INFO'].rstrip('/')
-
-@app.route('/')
-@view('index')
-def index():
-    return {}
-
 @app.route('/@')
 @app.route('/@/')
 @view('mail-index')
 def mail_index():
     return {}
-
-@app.route('/source/<filename:path>/')
-@app.route('/source/<filename:path>')
-def source(filename):
-    return static_file(filename, root = os.path.join(PORTAL_DIR, 'articles'))
-
-# Transition to this so I can host the static files with Apache directly.
-@app.route('/static/<fn:path>/')
-@app.route('/static/<fn:path>')
-def static(fn):
-    return static_file(fn, root = STATIC_DIR)
-@app.route('/img/<fn:path>/')
-@app.route('/img/<fn:path>')
-def static(fn):
-    return static_file(fn, root = os.path.join(STATIC_DIR, 'img'))
-@app.route('/css/<fn:path>/')
-@app.route('/css/<fn:path>')
-def static(fn):
-    return static_file(fn, root = os.path.join(STATIC_DIR, 'css'))
-@app.route('/js/<fn:path>/')
-@app.route('/js/<fn:path>')
-def static(fn):
-    return static_file(fn, root = os.path.join(STATIC_DIR, 'js'))
-
-
 
 @app.error(404)
 def error404(e):
--- dadaportal/urls.py
+++ dadaportal/urls.py
@@ -3,11 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dadaportal.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from .views import index
 
+urlpatterns = patterns('',
+    url(r'^/?$', index),
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^\+$', RedirectView.as_view(url='/+/')),
@@ -16,8 +15,6 @@ urlpatterns = patterns('',
     url(r'^@/', include('mail.urls')),
 
     url(r'^!/', include('articles.urls')),
-
-    # Static
-   #url(r'^static/',
-   #url(r'^img/.+', RedirectView.as_view(url='/static/img/')
+    url(r'^source/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.ARTICLES_DIR, 'show_indexes': True}),
 )
