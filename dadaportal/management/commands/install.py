2433f920197b0b96f7a62d52fedb5807c2735e75
Thomas Levine <_@thomaslevine.com>
2015-03-23 22:16:35 +0000

    -

--- dadaportal/management/commands/install.py
+++ dadaportal/management/commands/install.py
@@ -1,25 +1,7 @@
-import os, subprocess, sys
-
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
-SH_MESSAGE = '''You are about to run this command.
-
-    $ %s
-
-Are you sure you want to run it? Hit enter if yes.
-'''
-def sh(command):
-    sys.stdout.write(SH_MESSAGE % command)
-    input()
-    p = subprocess.Popen(command.split(), stdout = subprocess.PIPE,
-            stderr = subprocess.PIPE)
-    p.wait()
-    return p.stdout.read()
-
-def direction(command):
-    sys.stdout.write(command.strip() + '\n')
-    input()
+from ..util import sh, direction
 
 class Command(BaseCommand):
     args = '(none)'
