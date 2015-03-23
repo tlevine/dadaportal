ddb343bad96ee16e6616327133427373c874422c
Thomas Levine <_@thomaslevine.com>
2015-03-23 03:53:31 +0000

    disable mail explanation

--- mail/urls.py
+++ mail/urls.py
@@ -6,7 +6,7 @@ from .views import index, search, attachment
 urlpatterns = patterns('',
     # Empty query.
     url(r'^\s+/.*$', RedirectView.as_view(url='/@/')),
-    url(r'^$', index),
+    url(r'^$', RedirectView.as_view(url='/+/')),
 
     # Redirect to one with a slash.
   # url(r'^[^/]+$', RedirectView.as_view(url='')),
