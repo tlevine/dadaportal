799342ade7cc907adab708fb69cb7900497fd9f9
Thomas Levine <_@thomaslevine.com>
2015-03-24 22:11:46 +0000

    the management command

--- dadaportal/management/commands/deploy.py
+++ dadaportal/management/commands/deploy.py
@@ -1,16 +1,42 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
-from ..util import sh, direction
+def _run(args):
+    p = subprocess.Popen(args, stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE)
+    p.wait()
+    return p
+
+def rsync(local, remote, remote_host = 'nsa'):
+    return _run(['rsync', local, remote_host + ':' + remote])
+
+def ssh(command, remote_host = 'nsa'):
+    full_command = "cd '%s' && %s" % (settings.REMOTE_BASE_DIR, command)
+    return _run(['ssh', remote_host, full_command])
 
 class Command(BaseCommand):
     args = '(none)'
     help = 'This should be run from any account with appropriate SSH keys.'
 
     def handle(self, *args, **options):
-        direction('Run the tests.')
-        direction('Copy canonical articles to nsa.')
-        direction('Cache and index the articles on nsa.')
-        direction('Copy ``pal.conf`` and pal calendar files to nsa.')
-        direction('Generate static files on nsa.')
+        self.stdout.write('Running tests')
+        _run(['./manage.py', 'test'])
 
+        self.stdout.write('Copying canonical articles to nsa')
+        # no slash at end of remote directory
+        rsync('./canonical-articles/', '%s/canonical-articles' % settings.REMOTE_BASE_DIR)
+
+        self.stdout.write('Caching the articles on nsa')
+        ssh('./manage.py syncarticles')
+
+        self.stdout.write('Indexing the articles on nsa')
+        ssh('./manage.py indexarticles')
+
+        self.stdout.write('Copy pal.conf to nsa')
+        rsync(settings.LOCAL_PAL_CONF, settings.REMOTE_PAL_CONF)
+
+        self.stdout.write('Copying pal calendar files to nsa')
+        rsync(settings.LOCAL_PAL_DIR, settings.REMOTE_PAL_DIR)
+
+        self.stdout.write('Generating static files on nsa')
+        ssh('./manage.py collectstatic')
