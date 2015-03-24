9161fe381c91d802eb2f3fbb15344021c12fb218
Thomas Levine <_@thomaslevine.com>
2015-03-24 00:55:52 +0000

    install

--- articles/management/commands/indexarticles.py
+++ articles/management/commands/indexarticles.py
@@ -7,18 +7,20 @@ from django.template.loader import get_template
 
 from ...models import ArticleCache
 
+notmuch_dir = os.path.join(settings.NOTMUCH_DB, 'article')
+
 class Command(BaseCommand):
     args = '(none)'
-    help = 'Index articles in the notmuch database at %s.' % settings.NOTMUCH_DB
+    help = 'Index articles in the notmuch database at %s.' % notmuch_dir
 
     def handle(self, *args, **options):
         template = get_template('article-notmuch.html')
-        if os.path.isdir(settings.NOTMUCH_DB):
-            for thing in os.listdir(settings.NOTMUCH_DB):
-                os.remove(os.path.join(settings.NOTMUCH_DB, thing))
+        if os.path.isdir(notmuch_dir):
+            for thing in os.listdir(notmuch_dir):
+                os.remove(os.path.join(notmuch_dir, thing))
             subprocess.Popen(['notmuch', 'new'], stdout = subprocess.PIPE).wait()
         for article in ArticleCache.objects.all():
-            fn = os.path.join(settings.NOTMUCH_DB, article.endpoint.replace('/', '---'))
+            fn = os.path.join(notmuch_dir, article.endpoint.replace('/', '---'))
             dn = os.path.dirname(fn)
             os.makedirs(dn, exist_ok = True)
             with open(fn, 'w') as fp:
--- dadaportal/management/commands/install.py
+++ dadaportal/management/commands/install.py
@@ -1,23 +1,21 @@
+import sys
+
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
+from django.template import Context
+from django.template.loader import get_template
 
 from ..util import sh, direction
 
 class Command(BaseCommand):
     args = '(none)'
-    help = 'This should be run as the web user on the server.'
+    help = 'Generate an installation script to be run on the server.'
 
     def handle(self, *args, **options):
-        raise CommandError('This should be run on the web server.')
-
-        direction('''Install dependencies on nsa.
-  * Python libraries
-  * pal
-  * notmuch
-''')
-
-        # Create a user with the appropriate home directory and group.
-        sh('sudo createuser %s' % settings.WEB_USER)
+        template = get_template('install.sh')
+        with open(os.path.join(settings.BASE_DIR, 'requirements.txt')) as fp:
+            requirements = [line.strip() for line in fp]
+        
         
         direction('''
 Install this crontab to send public emails from the email server (home)
@@ -25,3 +23,10 @@ to nsa; you must copy it to the computer that contains your emails.''')
 
         direction('Set up the database on nsa.')
         direction('Install, configure and enable the Apache site on nsa.')
+        d = {
+            'requirements': requirements,
+            'web_user': settings.WEB_USER,
+            'notmuch_dir': os.path.join(settings.NOTMUCH_DB, 'mail'),
+        }
+
+        sys.stdout.write(template.render(Context(d)))
