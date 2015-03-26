3ff968a55aa6ce58aa4ec813811fe002ef1ac903
Thomas Levine <_@thomaslevine.com>
2015-03-26 00:39:07 +0000

    oops

--- dadaportal/settings.py
+++ dadaportal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.markup',
+   #'django.contrib.markup',
     'articles',
     'search',
     'mail',
