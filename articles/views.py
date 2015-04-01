dcf883ef2faee5585de25f50ea3c27a97ecc6a8b
Thomas Levine <_@thomaslevine.com>
2015-04-01 23:23:49 +0000

    support no tags

--- articles/views.py
+++ articles/views.py
@@ -23,9 +23,12 @@ def article(request, endpoint):
         return HttpResponseRedirect(a.redirect)
 
 def index(request):
-    tags = request.GET.get('tags', '')
-    qs = reduce(operator.or_, (Q(tag = tag) for tag in tags.split('|')))
-    query = ArticleTag.objects.filter(qs).select_related('article').distinct('article_id')
-    articles = [article_tag.article for article_tag in query if 'title' in article_tag.article.head()]
+    tags = request.GET.get('tags')
+    if tags == None:
+        query = ArticleTag.objects
+    else:
+        qs = reduce(operator.or_, (Q(tag = tag) for tag in tags.split('|')))
+        query = ArticleTag.objects.filter(qs).select_related('article')
+    articles = [article_tag.article for article_tag in query.distinct('article_id') if 'title' in article_tag.article.head()]
     articles_simple = [{'href': a.endpoint, 'title': a.head()['title']} for a in articles]
     return render(request, 'article-index.html', {'articles': articles_simple})
