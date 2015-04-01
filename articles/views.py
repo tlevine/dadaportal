4082b01e562fb7d7ead3abf52a81035061c05bac
Thomas Levine <_@thomaslevine.com>
2015-04-01 20:55:42 +0000

    redirect

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 
 from .models import ArticleCache
 
@@ -9,8 +9,11 @@ def article(request, endpoint):
         a = ArticleCache.objects.get(endpoint = endpoint)
     except ArticleCache.DoesNotExist:
         raise Http404('No such article')
-    else:
+
+    if a.redirect == None:
         params = a.head()
         params['modified'] = a.modified
         params['body'] = a.body
         return render(request, 'article.html', params)
+    else:
+        return HttpResponseRedirect(a.redirect)
