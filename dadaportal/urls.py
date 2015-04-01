ae7ee421a892e4679920e1e84633c5abc9b1012a
Thomas Levine <_@thomaslevine.com>
2015-04-01 03:24:09 +0000

    popular

--- dadaportal/urls.py
+++ dadaportal/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+from django.views.generic import TemplateView
 
 from .views import index, docs
 
@@ -9,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^/?$', index),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^docs/?$', docs),
+    url(r'^popular/?$', TemplateView.as_view(template_name = 'popular.html')),
 
     url(r'^\+$', RedirectView.as_view(url='/+/')),
     url(r'^\+/$', 'search.views.search'),
