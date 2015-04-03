8db4e133c4b2870700ed8acfd7d08828683b07d9
Thomas Levine <_@thomaslevine.com>
2015-04-03 10:48:19 +0000

    syncarticles

--- articles/management/commands/syncarticles.py
+++ articles/management/commands/syncarticles.py
@@ -27,49 +27,50 @@ def sync(subdir = (), threshold = None):
         threshold = settings.BEGINNING_OF_TIME
 
     parent = os.path.join(settings.ARTICLES_DIR, *subdir)
-    indexes = 0
+
+    # Process descendants
     for child in os.listdir(parent):
         fn = os.path.join(parent, child)
         if os.path.isdir(fn):
             yield from sync(subdir = subdir + (child,), threshold = threshold)
-        elif indexes > 0:
-            continue
         elif not os.path.isfile(fn):
             logger.warning('Skipping %s because it is a symlink' % fn)
             continue
 
-        indexes += 1
-        modified = datetime.datetime.fromtimestamp(os.stat(fn).st_mtime)
-        if not modified > threshold:
-            continue
-
-        endpoint = os.path.dirname(os.path.relpath(fn, settings.ARTICLES_DIR))
-        head, body, meta = from_file(os.path.join(settings.ARTICLES_DIR, endpoint))
+    # Process the present directory
+    indices = [os.path.join(parent, child) for child in os.listdir(parent) if child.startswith('index.')]
+    if len(indices) == 0:
+        logger.debug('No index file in "%s"' % parent)
+    elif datetime.datetime.fromtimestamp(os.stat(indices[0]).st_mtime) < threshold:
+        logger.debug('Index file "%s" is old, skipping' % indices[0])
+    else:
+        head, body, meta = from_file(parent)
         if head == None and body == None and meta == None:
-            logger.warn('I could not reify "%s", so I skipped it.' % endpoint)
-            continue
-        for k, v in head.items():
-            if isinstance(v, datetime.date):
-                head[k] = v.isoformat()
-
-        article_cache = ArticleCache.objects.filter(endpoint = endpoint)
-        path = os.path.join(settings.ARTICLES_DIR, endpoint, meta['filename'])
-        md5sum = hashlib.md5(open(path, 'rb').read()).hexdigest()
-
-        if article_cache.count() == 1:
-            article_cache.filter(~Q(md5sum = md5sum)).update(
-                filename = child, redirect = head.get('redirect'),
-                md5sum = md5sum,
-                modified = modified, headjson = json.dumps(head), body = body)
+            logger.warn('I could not reify "%s", so I skipped it.' % child)
         else:
-            article_cache = ArticleCache.objects.create(
-                filename = child, redirect = head.get('redirect'),
-                md5sum = md5sum,
-                endpoint = endpoint, modified = modified,
-                headjson = json.dumps(head), body = body)
+            for k, v in head.items():
+                if isinstance(v, datetime.date):
+                    head[k] = v.isoformat()
 
-        for tag in head.get('tags', []):
-            if ArticleTag.objects.filter(article = article_cache, tag = tag).count() == 0:
-                ArticleTag.objects.create(article = article_cache, tag = tag)
+            endpoint = os.path.relpath(parent, settings.ARTICLES_DIR)
+            article_cache = ArticleCache.objects.filter(endpoint = endpoint)
+            path = os.path.join(settings.ARTICLES_DIR, endpoint, meta['filename'])
+            md5sum = hashlib.md5(open(path, 'rb').read()).hexdigest()
 
-        yield endpoint
+            if article_cache.count() == 1:
+                article_cache.filter(~Q(md5sum = md5sum)).update(
+                    filename = child, redirect = head.get('redirect'),
+                    md5sum = md5sum, modified = meta['modified'],
+                    headjson = json.dumps(head), body = body)
+            else:
+                article_cache = ArticleCache.objects.create(
+                    endpoint = endpoint,
+                    filename = child, redirect = head.get('redirect'),
+                    md5sum = md5sum, modified = meta['modified'],
+                    headjson = json.dumps(head), body = body)
+
+            for tag in head.get('tags', []):
+                if ArticleTag.objects.filter(article = article_cache, tag = tag).count() == 0:
+                    ArticleTag.objects.create(article = article_cache, tag = tag)
+
+            yield endpoint
