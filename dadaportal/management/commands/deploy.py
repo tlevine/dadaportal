deb6da764471b2b9854e42e64bf409026091a79e
Thomas Levine <_@thomaslevine.com>
2015-04-13 08:14:52 +0000

    exclude in rsync

--- dadaportal/management/commands/deploy.py
+++ dadaportal/management/commands/deploy.py
@@ -12,7 +12,7 @@ def _run(args):
 
 def rsync(local, remote):
     r = '%s@%s:%s' % (settings.REMOTE_USER, settings.REMOTE_SSH_HOST, remote)
-    return _run(['rsync', '-avHS', '--exclude', '.*', local, r])
+    return _run(['rsync', '-avHS', '--exclude', 'static-compiled', '--exclude', '.*', local, r])
 
 def scp(local, remote):
     r = '%s@%s:%s' % (settings.REMOTE_USER, settings.REMOTE_SSH_HOST, remote)
