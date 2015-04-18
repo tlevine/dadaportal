0ac6f4c6ae30276594492d83bee2b2c1aa28c2b2
Thomas Levine <_@thomaslevine.com>
2015-04-18 06:17:41 +0000

    sync

--- articles/management/commands/syncarticles.py
+++ articles/management/commands/syncarticles.py
@@ -20,7 +20,10 @@ class Command(BaseCommand):
             n += 1
         self.stdout.write('Updated %d articles' % n)
 
-def sync(subdir = (), threshold = None):
+def sync(subdir = (), threshold = None, endpoints = set()):
+    if len(endpoints) == 0:
+        endpoints.update(row[0] for row in ArticleCache.objects.values_list('endpoint'))
+
     if threshold == None:
         threshold = ArticleCache.objects.all().aggregate(Max('modified'))['modified__max']
     if threshold == None: # (still)
@@ -37,11 +40,14 @@ def sync(subdir = (), threshold = None):
             logger.warning('Skipping %s because it is a symlink' % fn)
             continue
 
+    # Web endpoint
+    endpoint = os.path.relpath(parent, settings.ARTICLES_DIR)
+
     # Process the present directory
     indices = [os.path.join(parent, child) for child in os.listdir(parent) if child.startswith('index.')]
     if len(indices) == 0:
         logger.debug('No index file in "%s"' % parent)
-    elif datetime.datetime.fromtimestamp(os.stat(indices[0]).st_mtime) <= threshold:
+    elif datetime.datetime.fromtimestamp(os.stat(indices[0]).st_mtime) <= threshold or endpoint not in endpoints:
         logger.debug('Index file "%s" is old, skipping' % indices[0])
     else:
         head, body, meta = from_file(parent)
@@ -52,7 +58,6 @@ def sync(subdir = (), threshold = None):
                 if isinstance(v, datetime.date):
                     head[k] = v.isoformat()
 
-            endpoint = os.path.relpath(parent, settings.ARTICLES_DIR)
             article_cache = ArticleCache.objects.filter(endpoint = endpoint)
             path = os.path.join(settings.ARTICLES_DIR, endpoint, meta['filename'])
             md5sum = hashlib.md5(open(path, 'rb').read()).hexdigest()
