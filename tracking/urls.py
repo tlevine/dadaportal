e02b9f2ced441aed0279a1991a63fe602ae3f3df
Thomas Levine <_@thomaslevine.com>
2015-04-20 08:39:21 +0000

    ico

--- tracking/urls.py
+++ tracking/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from .views import analyze, followup_png, followup_js
+from .views import analyze, followup_ico, followup_js
 
 urlpatterns = patterns('',
     url(r'^$', analyze),
-    url(r'^followup\.png$', followup_png),
+    url(r'^followup\.ico$', followup_ico),
     url(r'^followup\.js$', followup_js),
 )
