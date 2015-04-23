995eac04a1ecb0851efb695faa779092006422c8
Thomas Levine <_@thomaslevine.com>
2015-04-23 21:47:57 +0000

    -

--- tracking/tests/test_context_processors.py
+++ tracking/tests/test_context_processors.py
@@ -19,10 +19,14 @@ def test_yes_user_agent():
     'Tracking should work with a user agent.'
     f = RequestFactory(HTTP_USER_AGENT = 'Django/1.7')
     request = f.get('/')
+    request.hit_id = 1
+    request.session = {'session_id': 3}
     assert list(tracking(request).keys()) == ['hit_id']
 
 @pytest.mark.django_db
 def test_no_user_agent():
     f = RequestFactory()
     request = f.get('/')
+    request.hit_id = 1
+    request.session = {'session_id': 3}
     assert list(tracking(request).keys()) == ['hit_id']
