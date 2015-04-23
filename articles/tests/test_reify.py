be9739cfdc0ce40efda209c2da2d41bd4261eeb5
Thomas Levine <_@thomaslevine.com>
2015-04-23 06:35:14 +0000

    test

--- articles/tests/test_reify.py
+++ articles/tests/test_reify.py
@@ -10,10 +10,18 @@ def test_from_file(tmpdir):
     assert head == {'parameter': 8230}
     assert body == 'I am a banana.\n'
     assert meta == {
+        'description': 'I am a banana.',
+        'facebook_description': 'I am a banana.',
+        'twitter_description': 'I am a banana.',
         'modified': os.stat(fp.strpath).st_mtime,
         'filename': 'index.txt',
         'redirect': None,
     }
 
 def test_parse():
-    parse(os.path.join('articles', 'tests', 'fixtures', '1.rst'))
+    head, body = parse(os.path.join('articles', 'tests', 'fixtures', '1.rst'))
+    assert head == {}
+    assert '<h1>Heading 1</h1>' in body
+    assert '<p>Paragraph</p>' in body
+    assert '<h2>Heading 2, a</h2>' in body
+    assert '<h2>Heading 2, b</h2>' in body
