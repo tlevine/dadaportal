a3667407a95398b0eb6ebaa8ff3573862a055dbb
Thomas Levine <_@thomaslevine.com>
2015-04-24 08:04:16 +0000

    -

--- old/schedule.disabled/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from .views import index, day, month
-
-urlpatterns = patterns('',
-    url(r'^$', index),
-    url(r'day/?$', day),
-    url(r'month/?$', month),
-)
