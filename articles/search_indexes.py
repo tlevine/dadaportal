f059168ac835a291b73a4e656d650b14be10050d
Thomas Levine <_@thomaslevine.com>
2015-04-25 08:48:27 +0000

    -

--- articles/search_indexes.py
+++ articles/search_indexes.py
@@ -5,7 +5,7 @@ from .models import Article
 
 class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
     date = indexes.DateTimeField(model_attr = 'modified')
-    title = indexes.CharField(model_attr = 'title')
+    title = indexes.CharField(model_attr = 'title', default = '')
     tags = indexes.MultiValueField(model_attr = 'tags')
 
     url = indexes.CharField(model_attr = 'endpoint')
