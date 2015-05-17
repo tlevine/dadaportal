3e1ac30b70d06e2b0677f1a2e81b1893794ff5fa
Thomas Levine <_@thomaslevine.com>
2015-05-17 14:52:03 +0000

    test img link

--- articles/tests/test_reify.py
+++ articles/tests/test_reify.py
@@ -9,3 +9,7 @@ def test_1_parse():
     assert '<p>Paragraph</p>' in body
     assert '<h2>Heading 2, a</h2>' in body
     assert '<h2>Heading 2, b</h2>' in body
+
+def test_img():
+    head, body = parse(os.path.join('articles', 'tests', 'fixtures', 'img.html'))
+    assert '<a href=a.png><img src=a.png' in body.replace('"', '')
