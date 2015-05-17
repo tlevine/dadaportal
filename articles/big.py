2548dd110ee664a5be3482333dd1eb551102e385
Thomas Levine <_@thomaslevine.com>
2015-05-17 15:24:07 +0000

    big

--- articles/big.py
+++ articles/big.py
@@ -1,9 +1,11 @@
 from lxml.html.clean import clean_html
 from lxml.html import fromstring, tostring
 
-TAGS = {'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-        'pre', 'img', 'table', 'blockquote',
-        'ol', 'ul', 'dl', 'ul', }
+SLIDE_TAGS = {'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+              'pre', 'img', 'table', 'blockquote',
+              'ol', 'ul', 'dl', 'ul', }
+
+WRAPPER_TAGS = {'div', 'a'}
 
 def guess_slides(body):
     '''
@@ -13,9 +15,9 @@ def guess_slides(body):
 
 def _subslides(element):
     result = b''
-    if element.tag in TAGS:
+    if element.tag in SLIDE_TAGS:
         result += b'<div>' + tostring(element).strip() + b'</div>'
-    elif element.tag == 'div':
+    elif element.tag in WRAPPER_TAGS:
         for subelement in element.getchildren():
             result += _subslides(subelement)
     return result
