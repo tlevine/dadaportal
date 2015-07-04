71e561afefeb419f6b760a5319eafd66b748807e
Thomas Levine <_@thomaslevine.com>
2015-07-04 18:34:44 +0000

    make video slides links

--- articles/big.py
+++ articles/big.py
@@ -1,9 +1,9 @@
 from lxml.html.clean import clean_html
-from lxml.html import fromstring, tostring
+from lxml.html import fromstring, tostring, Element
 
 SLIDE_TAGS = {'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
               'pre', 'img', 'table', 'blockquote',
-              'video', 'iframe',
+              'iframe',
               'ol', 'ul', 'dl', 'ul', }
 
 WRAPPER_TAGS = {'div', 'a', 'p'}
@@ -16,12 +16,20 @@ def guess_slides(body):
 
 def _subslides(element):
     result = b''
+
     if element.tag in SLIDE_TAGS:
-        # Add a space so images aren't backgrounds.
         result += b'<div>' + tostring(element).strip() + b'</div>'
+
+    elif element.tag == 'video' and 'src' in element.attrib:
+        href = element.attrib['src']
+        anchor = Element('a', href = href)
+        anchor.text = element.attrib.get('title', href)
+        result += b'<div>' + tostring(anchor).strip() + b'</div>'
+
     elif element.tag in WRAPPER_TAGS:
         for subelement in element.getchildren():
             result += _subslides(subelement)
+
     #   if element.tag == 'p':
     #       result += ('<notes>' + element.text_content() + '</notes>').encode('utf-8')
     return result
