4db083de17821abb9583f5eea76eb765feb009fe
Thomas Levine <_@thomaslevine.com>
2016-03-18 18:50:51 +0000

    default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,20 @@
 with import <nixpkgs> {}; {
-  dadaportalEnv = stdenv.mkDerivation {
-    name = "dadaportal";
-    buildInputs = [
-      python35
+  scott2 = stdenv.mkDerivation {
+    name = "scott2";
+    buildInputs = with pkgs; [
+      python35Packages.virtualenv
       python35Packages.pillow
       python35Packages.lxml
     ];
+    shellHook =
+    ''
+      if test $(basename "$PWD") != dadaportal; then
+        echo Run this from the dadaportal directory. > /dev/stderr
+        exit 1
+      fi
+      test -d .virtualenv || virtualenv-3.5 .virtualenv
+      . .virtualenv/bin/activate
+      pip install -r requirements.txt
+    '';
   };
 }
