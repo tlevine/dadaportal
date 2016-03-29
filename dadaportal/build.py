71473789dd659fbc877fc83fcaf1398de2b9f162
Thomas Levine <_@thomaslevine.com>
2016-03-29 22:58:28 +0000

    Automatic commit with j

--- dadaportal/build.py
+++ dadaportal/build.py
@@ -1,6 +1,33 @@
-def directory(x):
+import logging
+from collections import Counter
+
+from . import read, render
+
+logger = logging.getLogger(__name__)
+
+def build(x):
     if not os.path.isdir(x):
         raise TypeError('Not a directory: %s' % x)
-    for fn in os.listdir(x):
-        if os.path.isdir(x):
 
+    for y in os.listdir(x):
+        if os.path.isdir(y):
+            yield from build(os.path.join(x, y))
+
+    if _multiple_index_files(x):
+        logger.warn('''Multiple index files are in the directory "%s".
+I am processing neither.''' % x)
+    else:
+        for y in os.listdir(x):
+            if os.path.isfile(y):
+                z = read.file(os.path.join(x, y))
+                if z:
+                    yield z
+                    break
+        else:
+            tpl = 'No valid index files found the directory "%s".'
+            logger.warn(tpl % x)
+    
+
+def _multiple_index_files(x):
+    c = Counter(fn.split('.')[0] for fn in os.listdir(x))
+    return c['index'] > 1
