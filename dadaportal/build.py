836444c22c2a5c698eb63e905b80d7a19f4a6538
Thomas Levine <_@thomaslevine.com>
2016-03-29 23:16:39 +0000

    recursive build

--- dadaportal/build.py
+++ dadaportal/build.py
@@ -5,14 +5,23 @@ from . import read, render
 
 logger = logging.getLogger(__name__)
 
-def build(x, recursive=False):
+def build(src, dest, recursive:bool=False):
+    for srcfile, data in _read(src, recursive):
+        destfile = os.path.join(dest, os.path.relpath(srcfile, src))
+        if data:
+            raise NotImplementedError('Render to %s' % destfile)
+        else:
+            shutil.copy(srcfile, destfile)
+
+def _read(x, recursive):
     if not os.path.isdir(x):
         raise TypeError('Not a directory: %s' % x)
 
     if recursive:
         for y in os.listdir(x):
             if os.path.isdir(y):
-                yield from build(os.path.join(x, y))
+                yield from _read(os.path.abspath(os.path.join(x, y)),
+                                 recursive)
 
     if _multiple_index_files(x):
         logger.warn('''Multiple index files are in the directory "%s".
@@ -25,6 +34,8 @@ I am processing neither.''' % x)
                 if z:
                     yield fn, z
                     break
+                else:
+                    yield fn, None
         else:
             tpl = 'No valid index files found the directory "%s".'
             logger.warn(tpl % x)
