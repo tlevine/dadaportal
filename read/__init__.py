5e22e66bc85d4392679a747231c7ab0c51180fe5
Thomas Levine <_@thomaslevine.com>
2016-03-29 22:23:42 +0000

    Automatic commit with j

--- read/__init__.py
+++ read/__init__.py
@@ -13,3 +13,13 @@ def file(x):
         return None
 
     m.group(1)
+
+
+    title = models.TextField(null = True)
+    description = models.TextField(null = True)
+    body = models.TextField(null = False)
+
+    redirect = models.TextField(null = True)
+    tagsjson = models.TextField(null = False) # JSON
+    secret = models.BooleanField(null = False, default = False)
+    tags
--- read/models.py
+++
@@ -1,8 +0,0 @@
-    title = models.TextField(null = True)
-    description = models.TextField(null = True)
-    body = models.TextField(null = False)
-
-    redirect = models.TextField(null = True)
-    tagsjson = models.TextField(null = False) # JSON
-    secret = models.BooleanField(null = False, default = False)
-    tags
