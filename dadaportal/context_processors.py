e01119f4c80822402e1060b4c739058e4f85b76f
Thomas Levine <_@thomaslevine.com>
2016-03-29 22:15:22 +0000

    rm

--- dadaportal/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-def dadaportal(request):
-    return {
-        'site_title': settings.SITE_TITLE,
-        'domain_name': settings.DOMAIN_NAME,
-        'og_url': '%s://%s%s' % (settings.SCHEME, settings.DOMAIN_NAME, request.path_info),
-    }
