d6f8bd92a263f37a20c6cdbe15e882b8e7d70b99
Thomas Levine <_@thomaslevine.com>
2016-03-30 22:13:03 +0000

    Automatic commit with j

--- dadaportal/render/__init__.py
+++ dadaportal/render/__init__.py
@@ -1,20 +1,35 @@
 import os
+import datetime
 
 import lxml.html
+import jinja2
 
+from jinja2 import FileSystemLoader, Environment
 from . import html_utils, big_utils
 
+TEMPLATE_DIR = os.path.abspath(os.path.join(__file__, '..', 'templates'))
+ENV = Environment(loader = FileSystemLoader(TEMPLATE_DIR))
+
 def html(title, description, body):
+    tpl = ENV.get_template('html.html')
+
     x = lxml.html.fromstring(body.encode('utf-8'))
     y = html_utils.link_img(html_utils.link_headers(x))
-    z = lxml.html.tostring(y)
-    return z
-    raise NotImplementedError('Apply template.')
+    body = lxml.html.tostring(y)
+
+    now = datetime.datetime.now()
+
+    return tpl.render(title=title, description=description, body=body,
+                      modified=now.strftime('%Y-%m-%d %H:%M UTC'),
+                      modified_c=now.ctime())
 
 def slides(title, description, body):
+    tpl = ENV.get_template('big.html')
+
     x = lxml.html.fromstring(body.encode('utf-8'))
-    y = big_utils.subslides(x)
-    return y
+    body = big_utils.subslides(x)
+
+    return tpl.render(title=title, description=description, body=body)
 
 renderers = {
     'html': html,
