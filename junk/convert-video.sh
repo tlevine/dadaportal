5825cd6f9d118365a40962d4fa77552cadd45a2b
Thomas Levine <_@thomaslevine.com>
2016-04-04 15:35:55 +0000

    Automatic commit with j

---
+++ junk/convert-video.sh
@@ -0,0 +1,2 @@
+ffmpeg -i /lockers/tlevine_vol/s3-migration/home2/tlevine.bak-laptop/20110927-camera/DCIM/102CANON/MVI_5882.AVI
+/lockers/tlevine_vol/s3-migration/home2/tlevine.bak-laptop/20110927-camera/DCIM/uploaded11/MVI_4780.AVI
